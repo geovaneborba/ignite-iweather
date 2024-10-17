107877e02c1a5682e3fd1dcadbe743a92bc8cab3
Geovane Borba <geovanne@live.com>
2024-10-17 13:09:45 -0300

    test(Input): add tests for rendering activity indicator

---
+++ src/components/Input/input.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react-native";
+
+import { Input } from "@components/Input";
+
+describe("Component: Input", () => {
+  it("should be render without activity indicator", () => {
+    render(<Input />);
+
+    const activityIndicator = screen.queryByTestId("activity-indicator");
+
+    expect(activityIndicator).toBeNull();
+  });
+
+  it("should be render with activity indicator", () => {
+    render(<Input isLoading />);
+
+    const activityIndicator = screen.getByTestId("activity-indicator");
+
+    expect(activityIndicator).toBeTruthy();
+  });
+});
