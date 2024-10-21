f1ccfcee1d9649e2ba87a50229e97da4a079cb20
Geovane Borba <geovanne@live.com>
2024-10-21 09:22:15 -0300

    feat:(tests): add mocks and coverage

---
+++ __tests__/mocks/api/mockCityAPIResponse.ts
@@ -0,0 +1,13 @@
+import { CityAPIResponse } from "@services/getCityByNameService";
+
+export const mockCityAPIResponse: CityAPIResponse = {
+  id: "1",
+  name: "Gravataí",
+  sys: {
+    country: "BR",
+  },
+  coord: {
+    lon: -52.43,
+    lat: -29.98,
+  },
+};
